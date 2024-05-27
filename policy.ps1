a88af850b450ca53f205219184decd3cd1d40879
RetuArrow <126192521+RetuArrow@users.noreply.github.com>
2024-05-28 00:13:02 +0300

    Add files via upload
    added mandatory parameter: Tenant
    executes Remove-AzPolicyAssignment with confirmation prompt

--- policy.ps1
+++ policy.ps1
@@ -1,5 +1,10 @@
 # Remove "Default Azure Policy" from subscriptions in subscriptions in an Azure tenant
-$Tenant="azure-tenant-id-here"
+# Use parameter -Tenant to define the customer tenant for the subscriptions
+# e.g. policy.ps1 -Tenant aaa-bbb-ccc-ddd-eee
+param (
+[Parameter(Mandatory=$true)][string]$Tenant
+)
+
 
 # connect to Partner Center tenant account
 Write-Output "Connect with Azure account with sufficient RBAC access rights to subscription(s) in tenant"
@@ -18,13 +23,12 @@ foreach ($Sub in $Subscriptions) {
 	# Get-AzPolicyAssignment with name 'Default Azure Policy'
 	$Policy=Get-AzPolicyAssignment | Select-Object -Property Name -ExpandProperty properties | Select-Object -Property Name, PolicyDefinitionID, DisplayName | Where-Object { $_.DisplayName -eq "Default Azure Policy" }
 
-	write-output $Policy.Name
-	write-output $Policy.Displayname
-
-	if ( $Policy.PolicyDefinitionID -ne "") {
-		write-output "Would remove " +  $Policy.Name
-		# Remove the comment from the following statement to actually try and remove the policy assignment
-		#removeme  Remove-AzPolicyAssignment -Name $Policy.Name -Confirm:$false
+	if ( $Policy.PolicyDefinitionID -and $Policy.PolicyDefinitionID -ne "") {
+        write-output $Policy.Displayname
+		write-output "Asking to remove the policy: " $Policy.Name
+		# Ask the user to confirm before trying to remove the policy assignment
+		# Note! Will remove only the policy assignment, not the policy definition
+		Remove-AzPolicyAssignment -Name $Policy.Name -Confirm:$true
 	}
 }
 
